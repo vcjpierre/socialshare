47d84d485cd5f4688a6910c9ac443b062bed4fdf
Jean Pierre Velez <vcjpierre@gmail.com>
2021-12-26 02:27:00 -0400

    Create React app

---
+++ frontend/craco.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+};
